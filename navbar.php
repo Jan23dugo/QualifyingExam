48b4fd0e3fea3728f14ca710ec6963e843275402
Jan23dugo <jdugo23@gmail.com>
2024-10-13 07:12:03 +0800

    up

--- navbar.php
+++ navbar.php
@@ -5,8 +5,8 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="#">About Exam</a></li>
         <li><a href="#">Eligibility Checker</a></li>
-        <li><a href="#">Contact Us</a></li>
-        <li><a href="#">Take an Exam</a></li>
+        <li><a href="contact_us.php">Contact Us</a></li>
+        <li><a href="#take_exam.php">Take an Exam</a></li>
         <li><a href="register.php">Register</a></li>
     </ul>
 </nav>
