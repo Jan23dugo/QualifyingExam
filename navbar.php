c8cf83ed7b686bd14f8efcdb25798832a29cfe33
ydeoml <90219272+ydeoml@users.noreply.github.com>
2024-10-23 22:55:56 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,16 +1,23 @@
 <div class="navbar">
-        <img src="assets/img/PUP_CCIS_logo.png" alt="Left Logo" class="ccislogo">
-<nav>
-    <ul>
-        <li><a href="index.php">Home</a></li>
-        <li><a href="#">About Exam</a></li>
-        <li><a href="#">Eligibility Checker</a></li>
-        <li><a href="contact_us.php">Contact Us</a></li>
-        <li><a href="#take_exam.php">Take an Exam</a></li>
-        <li><a href="register.php">Register</a></li>
-    </ul>
-</nav>
-</div>
+        <img src="assets/img/ccislogo.png" alt="Left Logo" class="ccislogo">
+        <nav>
+            <ul>
+                <li><a href=index.php>HOME</a></li>
+                <li class="dropdown">
+                    <a href="javascript:void(0)" class="dropbtn">ABOUT EXAM</a>
+                    <div class="dropdown-content">
+                        <a href="about-exam.php#schedule">Exam Schedule</a>
+                        <a href="about-exam.php#requirements">Exam Requirements</a>
+                        <a href="about-exam.php#documents">Required Documents</a>
+                    </div>
+                </li>
+                <li><a href=eligibilitychecker.php>ELIGIBILITY CHECKER</a></li>
+                <li><a href="contactus.php">CONTACT US</a></li>
+                <li><a href=takeanexam.php>TAKE AN EXAM</a></li>
+                <li><a href=register.php>REGISTER</a></li>
+            </ul>
+        </nav>
+    </div>
 
 
        
