a932ff9486b591f2403773447a70295a663de70d
ydeoml <90219272+ydeoml@users.noreply.github.com>
2024-11-02 07:48:32 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,22 +1,21 @@
 <div class="navbar">
-        <img src="assets/img/ccislogo.png" alt="Left Logo" class="ccislogo">
-        <nav>
-            <ul>
-                <li><a href=index.php>HOME</a></li>
-                <li class="dropdown">
-                    <a href="javascript:void(0)" class="dropbtn">ABOUT EXAM</a>
-                    <div class="dropdown-content">
-                        <a href="about-exam.php#schedule">Exam Schedule</a>
-                        <a href="about-exam.php#requirements">Exam Requirements</a>
-                        <a href="about-exam.php#documents">Required Documents</a>
-                    </div>
-                </li>
-                <li><a href=eligibilitychecker.php>ELIGIBILITY CHECKER</a></li>
-                <li><a href=takeanexam.php>TAKE AN EXAM</a></li>
-                <li><a href=register.php>REGISTER</a></li>
-            </ul>
-        </nav>
-    </div>
-
+    <nav>
+        <ul>
+            <img src="assets/img/streamslogo.png" alt="STREAMS Logo" class="streamslogo">
+            <li><a href=index.php>HOME</a></li>
+            <li class="dropdown">
+                <a href="about-exam.php" class="dropbtn">ABOUT EXAM</a>
+                <div class="dropdown-content">
+                    <a href="about-exam.php#schedule">Exam Schedule</a>
+                    <a href="about-exam.php#requirements">Exam Requirements</a>
+                    <a href="about-exam.php#documents">Required Documents</a>
+                </div>
+            </li>
+            <li><a href=eligibilitychecker.php>ELIGIBILITY CHECKER</a></li>
+            <li><a href=takeanexam.php>TAKE AN EXAM</a></li>
+            <li><a href=registerFront.php>REGISTER</a></li>
+        </ul>
+    </nav>
+</div>
 
        
