8663af86636f4cc5ad8d43507269c34c4fa403a8
ydeoml <90219272+ydeoml@users.noreply.github.com>
2024-11-02 12:43:35 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+        <div class="footer-content">
+            <p>&copy; Polytechnic Univeristy of the Philippines. 2024 CCIS Qualifying Exam. All rights reserved.</p>
+        </div>
+    </footer>
